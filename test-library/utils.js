101a028378ebfb51002ae29511de406e42d9e8d3
Marc Specter laptop <marcspecter@marcs-air.home>
2017-08-21 13:14:03 +0100

    Asking a question with commit

--- test-library/utils.js
+++ test-library/utils.js
@@ -1,5 +1,6 @@
 var printOut = function(string, color) {
   var colors = {
+    //marc: Why reclare this array on every call? 
     red:   '\x1b[31m%s\x1b[31m',
     white: '\x1b[37m%s\x1b[37m',
     green: '\x1b[32m%s\x1b[32m',
