cbd54509e6a79e1b691771ca231982a6b9c9bccf
Chris Wickett <chris@chriswickett.com>
2017-08-21 13:19:38 +0100

    Don't declare vars on every call

--- test-library/utils.js
+++ test-library/utils.js
@@ -1,10 +1,10 @@
+var colors = {
+  red:   '\x1b[31m%s\x1b[31m',
+  white: '\x1b[37m%s\x1b[37m',
+  green: '\x1b[32m%s\x1b[32m',
+};
+
 var printOut = function(string, color) {
-  var colors = {
-    //marc: Why reclare this array on every call? 
-    red:   '\x1b[31m%s\x1b[31m',
-    white: '\x1b[37m%s\x1b[37m',
-    green: '\x1b[32m%s\x1b[32m',
-  };
   if (!colors.hasOwnProperty(color)) throw `Color not found: ${color}`;
   console.log(colors[color], string);
 };
